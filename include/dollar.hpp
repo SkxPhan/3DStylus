f63d330224f49695038c38557e8ba7d381ee42ba
SkxPhan <110777139+SkxPhan@users.noreply.github.com>
2024-03-09 22:21:13 +0900

    Add StrokeDetector class

--- include/dollar.hpp
+++ include/dollar.hpp
@@ -1,36 +1,44 @@
 #ifndef DOLLAR_PROTARCTOR_H_
 #define DOLLAR_PROTARCTOR_H_
 
+#include <string>
 #include <utility>
 #include <vector>
 
 namespace dollar {
-  using Point = std::pair<float, float>;
-  using VecItem = std::pair<float, float>;
-  using VectorizedStroke = std::vector<VecItem>;
+using Point = std::pair<float, float>;
+using VecItem = std::pair<float, float>;
+using VectorizedStroke = std::vector<VecItem>;
 
-  enum class Orientation {
-    Sensitive = 0,
-    Insensitive = 1,
-  };
-  // This represents a vectorized stroke
+enum class Orientation {
+  Sensitive = 0,
+  Insensitive = 1,
+};
+// This represents a vectorized stroke
 
-  class Stroke {
-    std::vector<Point> rawPoints;
-    VectorizedStroke vectorizedStroke;
+class Stroke {
+  std::vector<Point> rawPoints;
+  VectorizedStroke vectorizedStroke;
 
-  public:
-    // @param points: a vector of sampled points on this stroke.
-    // Must contains at least 2 points. No duplicated points allowed.
-    Stroke(const std::vector<Point>& points, Orientation orientationSensitivity,
-          int samplePointCnt = 16);
-    const std::vector<Point>& getRawPoints() const { return rawPoints; }
-    // This returns a VectorizedStroke with length 2n, where n
-    // is the number of input points
-    const VectorizedStroke& getVectorizedStroke() const {
-      return vectorizedStroke;
-    }
-  };
+ public:
+  // @param points: a vector of sampled points on this stroke.
+  // Must contains at least 2 points. No duplicated points allowed.
+  Stroke(const std::vector<Point>& points, Orientation orientationSensitivity,
+         int samplePointCnt = 16);
+  const std::vector<Point>& getRawPoints() const { return rawPoints; }
+  // This returns a VectorizedStroke with length 2n, where n
+  // is the number of input points
+  const VectorizedStroke& getVectorizedStroke() const {
+    return vectorizedStroke;
+  }
+};
+
+class StrokeDetector {
+  std::vector<std::pair<Stroke, std::string>> strokeTemplates;
+
+ public:
+  void addStrokeTemplate(Stroke strokeTemplate, std::string label);
+  void loadStrokeTemplates(std::string path);
 
   using Score = float;
 
@@ -39,7 +47,21 @@ namespace dollar {
   // This version never returns {templateEnd}
   template <typename ItT>
   std::pair<ItT, Score> recognize(const Stroke& target, ItT templateItBegin,
-                                  ItT templateItEnd);
+                                  ItT templateItEnd) {
+    float maxScore = 0;
+    ItT matchedIt = templateItEnd;
+    for (ItT templateIt = templateItBegin; templateIt != templateItEnd;
+         ++templateIt) {
+      const float d = optimalCosineDistance(target.getVectorizedStroke(),
+                                            templateIt->getVectorizedStroke());
+      const float score = 1.0f / d;
+      if (score > maxScore) {
+        maxScore = score;
+        matchedIt = templateIt;
+      }
+    }
+    return {matchedIt, maxScore};
+  }
 
   // Shorthand of the above function that returns {templateEnd}
   // when the most matching score is below {threshold}
@@ -53,33 +75,11 @@ namespace dollar {
     return it;
   }
 
-  namespace detail {
-    float optimalCosineDistance(const VectorizedStroke& stroke1,
-                                const VectorizedStroke& stroke2);
-  }
+ private:
+  float optimalCosineDistance(const VectorizedStroke& stroke1,
+                              const VectorizedStroke& stroke2);
+};
 
-  // impl
-  //
-  // @internal
-  // @{
-  template <typename ItT>
-  std::pair<ItT, Score> recognize(const Stroke& target, ItT templateItBegin,
-                                  ItT templateItEnd) {
-    float maxScore = 0;
-    ItT matchedIt = templateItEnd;
-    for (ItT templateIt = templateItBegin; templateIt != templateItEnd;
-        ++templateIt) {
-      const float d = detail::optimalCosineDistance(
-          target.getVectorizedStroke(), templateIt->getVectorizedStroke());
-      const float score = 1.0f / d;
-      if (score > maxScore) {
-        maxScore = score;
-        matchedIt = templateIt;
-      }
-    }
-    return {matchedIt, maxScore};
-  }
-  // @}
 }  // namespace dollar
 
 #endif
