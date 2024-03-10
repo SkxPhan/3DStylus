0551f2a908e568f716f175699e6b1d09b5592a51
SkxPhan <110777139+SkxPhan@users.noreply.github.com>
2024-03-10 21:51:13 +0900

    Improve testing

--- tests/testDollar.cpp
+++ tests/testDollar.cpp
@@ -7,9 +7,20 @@
 #include "dollar.hpp"
 using namespace dollar;
 using namespace std;
+
 TEST_CASE("1 instance") {
   vector<Stroke> strokes{
       Stroke({{0, 0}, {1, 1}, {1, 0}}, Orientation::Insensitive)};
+  auto [it, score] = recognize(strokes[0], strokes.begin(), strokes.end());
+  CHECK(it == strokes.begin());
+  CHECK_THAT(score, Catch::Matchers::WithinAbs(0.63662f, 0.001f));
+}
+
+TEST_CASE("2 instance") {
+  vector<Stroke> strokes{
+      Stroke({{0, 0}, {1, 1}, {1, 0}}, Orientation::Insensitive),
+      Stroke({{0, 0}, {0, 1}, {1, 1}}, Orientation::Insensitive),
+  };
   auto [it, score] = recognize(strokes[0], strokes.begin(), strokes.end());
   CHECK(it == strokes.begin());
   CHECK_THAT(score, Catch::Matchers::WithinAbs(0.63662f, 0.001f));
@@ -74,4 +85,26 @@ TEST_CASE("Regression of official data") {
       }
     }
   }
+}
+
+TEST_CASE("Optimal Cosine Distance Test", "[optimalCosineDistance]") {
+  VectorizedStroke stroke1 = {{0.5, 0.5}, {1.5, 1.5}, {2.5, 2.5}, {3.5, 3.5}};
+  VectorizedStroke stroke2 = {{0.7, 0.7}, {1.6, 1.6}, {2.6, 2.6}, {3.6, 3.6}};
+  float expectedDistance = 0.0375f;
+
+  float calculatedDistance = detail::optimalCosineDistance(stroke1, stroke2);
+
+  REQUIRE_THAT(calculatedDistance,
+               Catch::Matchers::WithinRel(expectedDistance));
+}
+
+TEST_CASE("Optimal Cosine Distance Symmetry Test", "[optimalCosineDistance]") {
+  VectorizedStroke stroke1 = {{0.5, 0.5}, {1.5, 1.5}, {2.5, 2.5}, {3.5, 3.5}};
+  VectorizedStroke stroke2 = {{0.7, 0.7}, {1.6, 1.6}, {2.6, 2.6}, {3.6, 3.6}};
+
+  float distance1 = detail::optimalCosineDistance(stroke1, stroke2);
+  float distance2 = detail::optimalCosineDistance(stroke2, stroke1);
+
+  // Check if distance is symmetric
+  REQUIRE_THAT(distance1, Catch::Matchers::WithinRel(distance2));
 }
