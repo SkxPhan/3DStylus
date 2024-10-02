0bb4badd9a2c28fde8febee19a55527c7996ccff
SkxPhan <110777139+SkxPhan@users.noreply.github.com>
2024-10-02 13:17:03 +0900

    Cleaning

--- src/main.cpp
+++ src/main.cpp
@@ -3,35 +3,33 @@
 #include "ImageProcessing.hpp"
 #include "StrokeDetector.hpp"
 
-using namespace cv;
-
 int main(int argc, char* argv[]) {
   const std::string sequencePath = "../data/ds536/rectangle_cw";
   std::vector<std::string> imageFiles;
-  glob(sequencePath + "/*depth.tif", imageFiles);
+  cv::glob(sequencePath + "/*depth.tif", imageFiles);
 
-  const std::string_view strokeTemplatePath = "tests/testdata";
+  const std::string_view strokeTemplatePath = "../data/templates";
   StrokeDetector detector;
   detector.loadTemplateStrokes(strokeTemplatePath);
 
   for (const auto& imageFile : imageFiles) {
-    Mat frame = cv::imread(imageFile, cv::IMREAD_GRAYSCALE);
+    cv::Mat frame = cv::imread(imageFile, cv::IMREAD_GRAYSCALE);
 
-    Mat handFrame;
+    cv::Mat handFrame;
     detectForegroundHand(frame, handFrame);
 
     cv::Point centroid = getCentroid(handFrame, false);
     detector.appendToBufferAndDetectStroke({centroid.x, centroid.y});
     drawCentroid(frame, centroid);
 
-    Mat combinedFrame;
+    cv::Mat combinedFrame;
     mergeFrames({frame, handFrame}, combinedFrame);
-    imshow("Depth video", combinedFrame);
+    cv::imshow("Depth video", combinedFrame);
 
-    if (waitKey(1000 / 30) == 27) break;
+    if (cv::waitKey(1000 / 30) == 27) break;
   }
 
-  destroyAllWindows();
+  cv::destroyAllWindows();
 
   return 0;
 }
